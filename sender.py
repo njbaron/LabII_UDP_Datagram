0461ecc053ea7ffb8eff76d78c8fc5fdc35c3f71
Nicholas Baron <njbaron@users.noreply.github.com>
2018-02-06 00:22:13 -0700

    sender works?

--- sender.py
+++ sender.py
@@ -12,6 +12,17 @@ def big_endian_ip(ip_string):
         big_endian.append(int(ip_string_arr[i]))
     return big_endian
 
+def ones_add(a, b):
+    a
+    b = (b[0]<<8) + b[1]
+    ret_int = a + b
+    print(a)
+    print(b)
+    while(ret_int > 0xffff):
+        remainder = ret_int >> 16
+        ret_int = (ret_int-0x10000) + remainder
+    return ret_int
+
 
 if not len(sys.argv) == 7:
     print("[ERROR} Command line aruments incorrect.")
@@ -21,34 +32,43 @@ if not len(sys.argv) == 7:
 """Reading Data"""
 keys = helpers.getKeys("test_keys_0")
 data = helpers.read(sys.argv[1])
-for i in range(0, len(keys)):
-    data = encrypt.encrypt(keys[i], data)
 
 data_length = len(data)
 
+for i in range(0, len(keys)):
+    data = encrypt.encrypt(keys[i], data)
+
 total_length = data_length + 20 #adding the length of the header.
-total_length = total_length.to_bytes(2, 'big')
 print(total_length)
+total_length = total_length.to_bytes(2, 'big')
 
 """Creating psuedo header"""
 packet = bytearray()
 packet += big_endian_ip(sys.argv[2])
 packet += big_endian_ip(sys.argv[3])
-packet.append(0x00)
-packet.append(17)
+packet += int(0).to_bytes(1, 'big')
+packet += int(17).to_bytes(1, 'big')
 packet += total_length
 
 """Creating UDP Header"""
 packet += int(sys.argv[4]).to_bytes(2, "big")
 packet += int(sys.argv[5]).to_bytes(2, 'big')
 packet += total_length
-packet.append(0x00) #adding space for the checksum value
-packet.append(0x00)
-packet += data
+packet += int(0).to_bytes(1, 'big') #adding space for the checksum value
+packet += int(0).to_bytes(1, 'big')
+#packet += data
 
 """Calculating checksum"""
+checksum = 0x00
 for i in range(0, len(packet)-2, 2):
-    
+    checksum = ones_add(checksum, packet[i:i+2])
+print(checksum)
+checksum = checksum ^ 0xffff
+print(checksum)
+checksum = checksum.to_bytes(2,'big')
+packet[18] = checksum[0]
+packet[19] = checksum[1]
+
 
 
 
